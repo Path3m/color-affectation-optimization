a8dea64f594120e81af4d54eab3d36b803fcd90e
Path3m <150528556+Path3m@users.noreply.github.com>
2024-05-06 18:45:36 +0200

    add of color on the boxplot

--- stat/BoxPlot.js
+++ stat/BoxPlot.js
@@ -1,25 +1,25 @@
 import { Permutation } from "../optigen/src/Permutation.js";
+import { ColorPalette } from "../graph/src/ColorPalette.js";
 
 export class OptigenBoxPlot{
-    static DEFAULT_COLORS = [
-        "rgb(0,0,0)",
-        "rgb(255,0,0)",
-        "rgb(0,255,0)",
-        "rgb(0,0,255)",
-    ];
-
     /**
      * Build the data of the box plot and set it ready to be drown
      * @param {*} div 
      * @param {*} geneticResult 
      */
     constructor(div, geneticResult){
-        let tmp = this.buildData(geneticResult.statistic);
+        let palette = ColorPalette.buildPalette(
+            20,{min:0.1,max:0.9},
+            d3.interpolateViridis
+        );
+
+        let tmp = this.buildData(geneticResult.statistic, palette.colors);
         let data = tmp.data;
         let range = tmp.range;
 
         this.div = div;
         this.data = data;
+
         this.layout = this.buildLayout(range);
     }
 
@@ -28,8 +28,7 @@ export class OptigenBoxPlot{
      * @param {*} statGenerations 
      * @returns 
      */
-    buildData(generationDatas){
-        var colors = OptigenBoxPlot.DEFAULT_COLORS;
+    buildData(generationDatas, colors){
         console.log(generationDatas);
 
         var xData = Permutation.index(generationDatas.length);
@@ -39,7 +38,7 @@ export class OptigenBoxPlot{
             max = Math.max(max, Math.max(...generationDatas[i]));
             min = Math.min(max, Math.min(...generationDatas[i]));
             var result = { type: 'box', boxmean: true, boxpoints: false,
-                line:{color:colors[i%colors.length]},
+                line:{color: colors[(i+5)%colors.length]},
                 y: generationDatas[i],
                 name: "Gen "+xData[i]};
             data.push(result);
