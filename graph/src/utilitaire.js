91eab13e303febc61c53c0730625087b6b6908a7
Path3m <150528556+Path3m@users.noreply.github.com>
2024-05-21 10:12:26 +0200

    add : function to get the range of value in a mtrix + linear interpolation

--- graph/src/utilitaire.js
+++ graph/src/utilitaire.js
@@ -156,3 +156,43 @@ export function closestProduct(num) {
 export function isInt(x){
     return typeof x === 'number' && Math.floor(x) === x;
 }
+
+/**
+ * Get the min and max value in a matrix
+ * @param {*} matrix 
+ * @returns an array of two element : index 0 is the min value and index 1 the max
+ */
+export function matrixRangeValue(matrix){
+    if(matrix !== undefined){
+        let min = matrix[0][0];
+        let max = matrix[0][0];
+
+        for(let i=0; i<matrix.length; i++){
+            for(let j=0; j<matrix[0].length; j++){
+                min = (matrix[i][j] < min) ? matrix[i][j] : min;
+                max = (matrix[i][j] > max) ? matrix[i][j] : max;
+            }
+        }
+        
+        return [min, max];
+    }else{
+        return [];
+    }
+}
+
+/**
+ * Linear interpolation of a number between two range
+ * @param {*} number 
+ * @param {*} range1 the given number belongs to range1
+ * @param {*} range2 the returned number belongs to range2
+ * @returns 
+ */
+export function interpolate(number, range1, range2){
+    let y1 = Math.min(...range2); let y2 = Math.max(...range2);
+    let x1 = Math.min(...range1); let x2 = Math.max(...range1);
+
+    let a = (y2-y1)/(x2-x1);
+    let b = y1-a*x1;
+
+    return number * a + b;
+};
