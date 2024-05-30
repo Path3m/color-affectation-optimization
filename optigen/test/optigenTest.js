ddc2d45611cfc7dbe4f8f3864c0a738054585c47
Path3m <150528556+Path3m@users.noreply.github.com>
2024-05-30 15:00:57 +0200

    add function to execute optimisation in accordance to the number entered in the field of importance html page

--- optigen/test/optigenTest.js
+++ optigen/test/optigenTest.js
@@ -3,6 +3,7 @@ import { Permutation } from "../src/Permutation.js";
 import { Population } from "../src/Population.js";
 
 import { OptigenBoxPlot } from "../../stat/BoxPlot.js";
+import * as obj from "../../global.js";
 
 //SCORE COMPUTING to test the genetic optimisation
 function bestScoreIfOrdered(array) {
@@ -26,13 +27,11 @@ function bestScoreIfEvenFirst(array){
 
 const score = bestScoreIfEvenFirst;
 
-console.clear();
-console.log("on commence ici *************************************************\n");
+//INITIALISATION --------------------------------------------------------------------------------
+const default_dimensions = {limit: 30, generation: 200, individual: 20};
+const default_factors    = {reproduction: 0.6, mutation: 0.5, selection: 0.5};
 
-let dimensions = {limit: 30, generation: 200, individual: 20};
-let factors    = {reproduction: 0.6, mutation: 0.5, selection: 0.5};
-
-let optigen = new Optigen(score, dimensions, factors);
+let optigen = new Optigen(score, default_dimensions, default_factors);
 
 console.time('a');
 let result = optigen.execute();
@@ -45,10 +44,45 @@ popfin.poplog();
 
 console.log("Meilleur élément : ", popfin.members[0].genome, " | ", popfin.members[0].score);
 
-let boxplot = new OptigenBoxPlot(result, "optigen");
-boxplot.draw(dimensions, factors, popfin.members[0]);
+let boxplot = new OptigenBoxPlot(result, default_dimensions, default_factors, "optigen");
+boxplot.draw();
 
 let meilleur = [18,16,14,12,10,8,6,4,2,0,1,3,5,7,9,11,13,15,17,19];
 console.log("Score maximum : ",meilleur,"|",score(meilleur));
 
-console.log("\non termine là ***************************************************");
+//------------------------------------------------------------------------
+
+window.executeOptimisation = () => {
+    var limit = document.getElementById("number-gen").value;
+    var generation = document.getElementById("size-pop").value;
+    var individual = document.getElementById("size-individual").value;
+    var repro = document.getElementById("percent-repro").value;
+    var selec = document.getElementById("percent-selec").value;
+    var mutation = document.getElementById("percent-mut").value;
+
+    let dimensions = {
+        limit: (limit === "") ? default_dimensions.limit : limit,
+        generation: (generation === "") ? default_dimensions.generation : generation,
+        individual: (individual === "") ? default_dimensions.individual : individual
+    };
+
+    let factors = {
+        reproduction: (repro === "") ? default_factors.reproduction : repro,
+        mutation: (mutation === "") ? default_factors.mutation : mutation,
+        selection: (selec=== "") ? default_factors.selection : selec
+    };
+
+    console.log(dimensions); console.log(factors);
+    optigen = new Optigen(score, dimensions, factors);
+
+    var start = new Date().getTime();
+    result = optigen.execute();
+    var end = new Date().getTime();
+    var duration = start - end;
+
+    renewElement("optigen");
+    let boxplot = new OptigenBoxPlot(result, dimensions, factors, "optigen");
+    boxplot.draw();
+
+    document.getElementById("optigen").innerHTML += "Executé en : "+duration+" ms";
+}
